deacfec663e3e0254a23348f5d29d18844b48f25
Your NameJoey <joeylinnn98@gmail.com>
2026-04-02 01:42:20 -0400

    manually bind SmtpSettings

--- api/Program.cs
+++ api/Program.cs
@@ -14,8 +14,17 @@ builder.Services
     .AddApplicationInsightsTelemetryWorkerService()
     .ConfigureFunctionsApplicationInsights();
 
-builder.Services.Configure<SmtpSettings>(
-    builder.Configuration.GetSection("SmtpSettings"));
+// builder.Services.Configure<SmtpSettings>(
+//     builder.Configuration.GetSection("SmtpSettings"));
+builder.Services.Configure<SmtpSettings>(options =>
+{
+    options.Host = builder.Configuration["SmtpHost"] ?? "";
+    options.Port = int.TryParse(builder.Configuration["SmtpPort"], out var port) ? port : 0;
+    options.UserName = builder.Configuration["SmtpUserName"] ?? "";
+    options.Password = builder.Configuration["SmtpPassword"] ?? "";
+    options.FromEmail = builder.Configuration["SmtpFromEmail"] ?? "";
+    options.FromName = builder.Configuration["SmtpFromName"] ?? "";
+});
 
 builder.Services.AddScoped<ILoanRepository,LoanRepository>();
 builder.Services.AddScoped<LoanCalculator>();
